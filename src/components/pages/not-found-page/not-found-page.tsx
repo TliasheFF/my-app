4dc04bcfcca2498cd0c83c03f080e0864551494d
Tliashev.FF <f.tlyashev@gmail.com>
2024-02-22 22:02:13 +0500

    fix: поправил стили для страницы 404

--- src/components/pages/not-found-page/not-found-page.tsx
+++ src/components/pages/not-found-page/not-found-page.tsx
@@ -1,16 +1,16 @@
 import { FC } from "react";
 import styles from "./styles.module.scss";
 import { Link } from "react-router-dom";
+import { Button } from "../../button/button";
 
 export const NotFoundPage: FC = () => {
   return (
     <div className={styles.p404}>
       <img src="/src/assets/404.png" className={styles.p404__image} />
-      <p className={styles.p404__text}>
-        404 <span className={styles.p404__line}>|</span> Страница не найдена...
-      </p>
-      <Link to="/" className={styles.p404__link}>
-        Вернуться на главную страницу
+      <p className={styles.p404__code}>404</p>
+      <p className={styles.p404__text}>Кажется, такой страницы не существует...</p>
+      <Link to="/">
+        <Button>Вернуться на главную страницу</Button>
       </Link>
     </div>
   );
