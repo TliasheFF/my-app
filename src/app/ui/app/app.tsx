574cbddf13054e1dd85a2f3f2f04438865b998dd
Tliashev.FF <tliashev.ff@parma.ru>
2024-05-25 19:38:16 +0500

    Переместил theme в папку config

--- src/app/ui/app/app.tsx
+++ src/app/ui/app/app.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import "./app.scss";
 import { router } from "@/app/router";
 import { ConfigProvider } from "antd";
-import { theme } from "@/shared/theme";
+import { theme } from "@/shared/config/theme";
 
 export const App = () => {
   return (
